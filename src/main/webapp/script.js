1edf168c293e3a0118ee11a4d04637a9af4bb252
James Tan <jamestancheemin98@gmail.com>
2020-08-08 22:01:52 +0800

    Fix No Restaurant Found, Add animation for next search
    This commit:
    -fixes the bug whereby no restaurant found wasn't being shown.
    -replaces hiding result container with collapsing of result container
     upon pressing search.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -93,7 +93,8 @@ function initMap() {
     }
 
     resizeNavButton(keepCollapsed=true);
-    document.getElementById("result-container").classList.add("hide");
+    setRestaurantTab(hidden=true);
+    resultHidden=true;
 
     const url = new URL('/places-list', window.location.origin),
           params = {
@@ -124,7 +125,6 @@ let restaurants;
 let restaurantIndex = 0;
 function updateRestaurant(result) {
   let resContainerElement = document.getElementById("result-container");
-  resContainerElement.style.display = "block";
 
   shuffleArray(result);
   restaurants = result;
@@ -136,17 +136,7 @@ function updateRestaurant(result) {
     document.getElementById("restaurant-details").style.display = "flex";
     document.getElementById("no-result-text").style.display = "none";
 
-    resContainerElement.classList.remove("hide");
-    resContainerElement.scrollTop = 0;
-    document.getElementById("infowindow-restaurant-content").classList.remove("hide");
-
     updateRestaurantDetails(0);
-
-    // Show restaurant tab if hidden
-    if(resultHidden) {
-      resultHidden = false;
-      setRestaurantTab(resultHidden);
-    }
 
     // Hide next and previous buttons if only one restaurant
     if(result.length === 1) {
@@ -154,6 +144,16 @@ function updateRestaurant(result) {
     } else {
       document.getElementById("restaurant-buttons").style.display = "flex";
     }
+    resContainerElement.classList.remove("hide");
+    resContainerElement.scrollTop = 0;
+    document.getElementById("infowindow-restaurant-content").classList.remove("hide");
+  }
+  resContainerElement.classList.remove("hide");
+  
+  // Show restaurant tab if hidden
+  if(resultHidden) {
+    resultHidden = false;
+    setRestaurantTab(resultHidden);
   }
 }
 
@@ -254,7 +254,7 @@ document.getElementById("next-button").addEventListener("click", function() {
   nextRestaurant();
 });
 
-let resultHidden = false;
+let resultHidden = true;
 document.getElementById("result-hide-button").addEventListener("click", function() {
   resultHidden = !resultHidden;
   setRestaurantTab(resultHidden);
@@ -369,6 +369,7 @@ function resizeNavButton(keepCollapsed=false) {
     expandButton.classList.replace("show", "hide");
     collapseButton.classList.replace("hide", "show");
     setRestaurantTab(hidden=true);
+    resultHidden = true;
   }
 }
 
